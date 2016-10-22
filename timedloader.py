d1b5842299a5af621a58bfe20fb5d126a0953d13
Alex <acwang@berkeley.edu>
2016-10-22 01:02:19 -0700

    something

--- timedloader.py
+++ timedloader.py
@@ -3,11 +3,26 @@ import webbrowser
 #Let's also make a poem-generator! Anyway...
 
 url = "www.google.com"
-# webbrowser.open_new(url)
-
+urls = [url, "www.yahoo.com", "www.youtube.com"]
 array = str(datetime.now()).split(" ")
-date = array[0]
-time = array[1]
+currtime = array[1][:5]
+print(currtime)
 
-print(date)
-print(time)
+
+data = {"00:29:00" : (url, "www.yahoo.com", "www.youtube.com"),
+        "00:30:00" : ("www.twitter.com", "www.stackexchange.com")}
+
+
+#Things to do:
+# 1) Data structure
+# example of a tuple 
+
+# for s in urls:
+#     webbrowser.open_new(s)
+
+
+
+# 2) Input reader
+# 3) webbrowser opens URL
+# 4) Write to file
+# 5) 
